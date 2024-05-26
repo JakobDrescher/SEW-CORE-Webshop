28919b8f56de47e2d9849a155c8cfb5749c6e2f1
JakobDrescher <jakobdrescher6@gmail.com>
2024-05-26 18:29:09 +0200

    added basic functionality for displaying products

---
+++ src/components/Product.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="card col-2 m-3">
+  <img :src="img" class="card-img-top object-fit-cover">
+  <div class="card-body p-3">
+    <h3 class="card-title">{{name}}</h3>
+    <h4 class="card-subtitle">{{price}}€</h4>
+    <p class="card-text">{{description}}</p>
+  </div>
+</div>
+</template>
+
+<script setup>
+const props = defineProps(['name','price','description','img'])
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,9 @@
+<template>
+  <product v-for="p in productStore.products" :name="p.name" :description="p.description" :price="p.price" :img="p.img"></product>
+</template>
+
+<script setup>
+import Product from "@/components/Product.vue";
+import {useProductStore} from "@/stores/productStore.js";
+const productStore=useProductStore();
+</script>

ff497dab30e52f4abe2d8156638d5bb12d57e37d
JakobDrescher <jakobdrescher6@gmail.com>
2024-05-26 17:24:38 +0200

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+    </nav>
+  </header>
+  <main>
+    <RouterView/>
+  </main>
+</template>
